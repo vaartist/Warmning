25465f4907783aa4360eead9c7fa339c54f104db
Oscar Esquivel <oscar12esquivel@gmail.com>
2014-11-14 16:47:18 -0600

    Consulta 2 en progreso.

--- Implementacion/Consultas.sql
+++ Implementacion/Consultas.sql
@@ -30,6 +30,10 @@ SELECT	*	FROM	Canton	WHERE	Nombre='HEREDIA';
 --Problemas a resolver:
 --1.Cómo obtener las coordenadas de cualquier punto en el país de una forma fácil, por ejemplo usando el mapa.
 --  Por ahora se toman coordenadas de estaciones de bomberos y se modifican ligeramente para simular locaciones.
+--2.Cómo implementar la función de CalcularDistanciaReal. Se puede usar el algoritmo de Dijkstra para determinar
+--  el camino más corto entre dos punto, pero éste necesita nodos en cada vértice, y los caminos de esta BD sólo
+--  se intersecan, no poseen nodos en las intersecciones. En cuanto al peso de cada arista, se usaría la distancia
+--  del camino.
 
 
 
@@ -62,11 +66,46 @@ SET			@Localizacion = GEOMETRY::Point(478543.64500605746, 1106318.5944643875, 0)
 --SET		@Localizacion = GEOMETRY::Point(, , 0)
 --SET		@Localizacion = GEOMETRY::Point(, , 0)
 SELECT		TOP 3
-			Nombre							AS	'Nombre de la estación',
-			Direccion						AS	'Dirección de la estación',
-			@Localizacion.STDistance(Geom)	AS	'Distancia en línea recta (metros)',	--Considerar usar ROUND para redondear la distancia
-			CodigoDistrito					AS	'Código del distrito',
-			Geom.ToString()					AS	'Ubicación en el mapa'					--Opcional
+			Nombre										AS	'Nombre de la estación',
+			Direccion									AS	'Dirección de la estación',
+			CalcularDistanciaReal(@Localizacion, Geom)	AS	'Distancia en línea recta (metros)',	--Considerar usar ROUND para redondear la distancia
+			CodigoDistrito								AS	'Código del distrito',
+			Geom.ToString()								AS	'Ubicación en el mapa'					--Opcional
 FROM		Estacion_Bomberos
-ORDER BY	@Localizacion.STDistance(Geom)
+ORDER BY	CalcularDistanciaReal(@Localizacion, Geom)
 --Fin de consulta
+
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+--Consulta 3: Obtener nivel de peligro de incendio en cualquier punto del país, tomando en cuenta zonas de riesgo y distancia a las estaciones de bomberos más cercanas con distancia real.
+--Se muestran los datos de manera formal para la consulta.
+--Fin de consulta
+
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+--Consulta 4: Obtener "área de influencia" de una estación de bomberos (mostrar según distancias de caminos el área que puede cubrir rápidamente).
+--Se muestran los datos de manera formal para la consulta.
+--Fin de consulta
+
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+--Consulta 5: Obtener la unión de esas áreas de influencia para determinar lugares en el país que no estén dentro de ninguna, para recomendar creación de nuevas estaciones.
+--Se muestran los datos de manera formal para la consulta.
+--Fin de consulta
+
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+--Función para calcular distancia real (según caminos) entre dos puntos.
+CREATE FUNCTION dbo.CalcularDistanciaReal
+(
+	@LocalizacionA	GEOMETRY,
+	@LocalizacionB	GEOMETRY
+)
+RETURNS	INT
+AS
+BEGIN
+	DECLARE	@DistanciaReal	INT
+	RETURN	@DistanciaReal
+END
+--Fin de función
+--DROP FUNCTION dbo.CalcularDistanciaReal
