575b5055a52c86d24e6049354f7d605cc873cd58
Oscar <oscar12esquivel@gmail.com>
2014-11-21 10:02:54 -0600

    Procedimiento para borrar geometrías repetidas de caminoTmp2.

---
+++ Implementacion/Borrar.sql
@@ -0,0 +1,27 @@
+USE DW_user4;
+
+--
+
+SELECT COUNT(*) FROM caminoTmp2
+--102 760 tuplas antes de borrar...
+-- 66 657 tuplas después de borrar...
+
+--Borrar calles con la misma geometría, dejar sólo una
+DECLARE @ID		INT,
+		@Geom	GEOMETRY
+--Declararar el cursor
+DECLARE	cursor_tabla CURSOR FOR
+SELECT		ID, geom
+FROM		caminoTmp2
+--Abrir cursor y usar FETCH
+OPEN cursor_tabla
+FETCH cursor_tabla INTO @ID, @Geom
+WHILE(@@FETCH_STATUS = 0)
+BEGIN
+	DELETE	FROM caminoTmp2
+	WHERE	@Geom.STEquals(geom) = 1 AND ID != @ID
+	FETCH NEXT FROM cursor_tabla INTO @ID, @Geom
+END
+--Cerrar cursor
+CLOSE cursor_tabla
+DEALLOCATE cursor_tabla
